ddd3d6de12c03dcbf6fc11d96df628c8414587b4
Dan Nixon <dan@dan-nixon.com>
2019-02-28 09:37:02 +0000

    Test for enum schema

--- savu/core/parameters/schemas/enum.py
+++ savu/core/parameters/schemas/enum.py
@@ -9,11 +9,12 @@ class Enum(Schema):
 
     def validate(self, value):
         super(Enum, self).validate(value)
-        if value not in self._enum_values:
+
+        if not (value is None or value in self._enum_values):
             raise SchemaValidationError('Value "{}" not in allowed enum items ({})'.format(value, self._enum_values))
 
     def value_help_str(self):
-        text = 'One of {}'.format(','.join(self._enum_values))
+        text = 'One of {}'.format(', '.join(['"{}"'.format(v) for v in self._enum_values]))
         if self._nullable:
             text += ', or None'
         return text
---
+++ savu/core/parameters/schemas/enum_test.py
@@ -0,0 +1,41 @@
+import unittest
+
+from .. import SchemaValidationError
+from .enum import Enum
+
+
+class EnumTest(unittest.TestCase):
+
+    def test_help_text_nullable(self):
+        s = Enum(['a', 'b', 'c'], nullable=True)
+        self.assertEquals(s.value_help_str(), 'One of "a", "b", "c", or None')
+
+    def test_help_text_not_nullable(self):
+        s = Enum(['a', 'b', 'c'], nullable=False)
+        self.assertEquals(s.value_help_str(), 'One of "a", "b", "c"')
+
+    def test_validate_nullable(self):
+        s = Enum(['a', 'b', 'c'], nullable=True)
+
+        # All valid
+        s.validate(None)
+        s.validate('a')
+        s.validate('b')
+        s.validate('c')
+
+        # Not valid
+        with self.assertRaises(SchemaValidationError):
+            s.validate('d')
+
+    def test_validate_not_nullable(self):
+        s = Enum(['a', 'b', 'c'], nullable=True)
+
+        # All valid
+        s.validate('a')
+        s.validate('b')
+        s.validate('c')
+
+        # Not valid
+        with self.assertRaises(SchemaValidationError):
+            s.validate(None)
+            s.validate('d')
